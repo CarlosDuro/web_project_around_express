acc8f49fba88c385d2c8d68f95fa6c726f748055
CarlosDuro <durancarlosduran@gmail.com>
2025-07-28 18:58:24 -0600

    Cambio app.js por recurso solicitado no encontrado en Render

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const PORT = process.env.PORT || 3000;
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
 
+// Middleware para parsear JSON
 app.use(express.json());
 
 // Usar routers en rutas específicas
 app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
 
-// Ruta raíz con mensaje de bienvenida
+// Ruta raíz con mensaje de bienvenida y lista de endpoints
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "API de Carlos Durán funcionando correctamente 🚀" });
+  res.status(200).json({
+    message: "API de Carlos Durán funcionando correctamente 🚀",
+    endpoints: ["/users", "/users/:id", "/cards"],
+  });
 });
 
 // Manejar rutas no definidas
@@ -24,6 +26,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Recurso solicitado no encontrado" });
 });
 
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
